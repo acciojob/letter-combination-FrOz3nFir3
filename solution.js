af5af296105aadd87c58534a95cc079fe2b2930b
Kush Surti <30488380+FrOz3nFir3@users.noreply.github.com>
2022-11-24 12:39:03 +0530

    brute force approach

--- solution.js
+++ solution.js
@@ -1,5 +1,32 @@
 function letterCombinations(input_digit) {
-  //Complete the function
+  //brute force approach (assuming only two digits)
+  var numberStringHt = {
+    2:"abc",
+    3:"def",
+    4:"ghi",
+    5:"jkl",
+    6:"mno",
+    7:"pqrs",
+    8:"tuv",
+    9:"wxyz",
+  }
+
+  var allCombinations = [];
+
+  for (let i = 0; i < input_digit.length; i++) {
+    let currentString = numberStringHt[input_digit[i]];
+    let nextString = numberStringHt[input_digit[i+1]];
+
+    if(nextString){
+      for (const currentLetter of currentString) {
+        for (let nextLetter of nextString) {
+          allCombinations.push(currentLetter + nextLetter)
+        }
+      }
+    }
+  }
+
+  return allCombinations;
 }
 
 module.exports = letterCombinations;
